418c2cb3725d08f3e97fdc96cd1c45898283ad6f
Daniel Martí <mvdan@mvdan.cc>
2022-05-31 17:44:23 +0100

    cmd/cueckoo: implement new importpr command
    For a pull request with number PRID, it does:
    
    * Check that the branch name importpr-PRID does not exist
    * Fetch PRID's HEAD into the branch above
    * Rebase and squash the PR's commits on the latest master tip on github.com
    * Drop the user into their editor to fix up the commit message
    
    It then leaves the user in the branch, ready to call git-codereview mail.
    
    There are a few TODOs left, but this is a good first cut.
    
    Signed-off-by: Daniel Martí <mvdan@mvdan.cc>

--- cmd/cueckoo/cmd/importpr.go
+++ cmd/cueckoo/cmd/importpr.go
@@ -15,10 +15,14 @@
 package cmd
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"os"
+	"os/exec"
 	"strconv"
+	"time"
 
-	"github.com/google/go-github/v31/github"
 	"github.com/spf13/cobra"
 )
 
@@ -33,6 +37,9 @@ func newImportPRCmd(c *Command) *cobra.Command {
 }
 
 func importPRDef(c *Command, args []string) error {
+	log.SetPrefix("[importpr] ")
+	log.SetFlags(0) // no timestamps, as they aren't very useful
+
 	cfg, err := loadConfig()
 	if err != nil {
 		return err
@@ -48,28 +55,86 @@ func importPRDef(c *Command, args []string) error {
 		return fmt.Errorf("%q is not a valid number", pr)
 	}
 
-	// TODO: validate that the number provided is indeed a valid PR
+	log.Printf("using github remote URL %q", cfg.githubURL)
 
-	// TODO: work out support for multiple PRs
+	branchName := fmt.Sprintf("importpr-%d", pr)
 
-	msg := fmt.Sprintf("Import PR %d from GitHub to Gerrit", pr)
-	payload, err := buildImportPRPayload(msg, importPRPayload{
-		PR: pr,
-	})
-	if err != nil {
+	// TODO: Note that mainErr's ctx is not wired here.
+	// We should wire it up and use it for e.g. FetchContext.
+	// For now, use a hard-coded timeout of 10s.
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	// If the branch already exists, refuse to continue.
+	if out, err := run(ctx,
+		"git", "show-ref", "--verify", "--quiet", fmt.Sprintf("refs/heads/%s", branchName),
+	); err == nil {
+		return fmt.Errorf("branch %q already exists; delete it to start over", branchName)
+	} else if len(out) == 0 {
+		// An error without output means the branch does not exist.
+	} else {
+		return err // something else went wrong
+	}
+
+	// Fetch the PR HEAD and place it in a new branch, then switch to it.
+	if _, err := run(ctx,
+		"git", "fetch", "--quiet", cfg.githubURL,
+		fmt.Sprintf("pull/%d/head:%s", pr, branchName),
+	); err != nil {
 		return err
 	}
-	err = cfg.triggerRepositoryDispatch(cfg.githubOwner, cfg.githubRepo, payload)
-	if err != nil {
+	if _, err := run(ctx, "git", "switch", "--quiet", branchName); err != nil {
 		return err
 	}
+	log.Printf("fetched PR into branch %q", branchName)
+
+	// Rebase all the commits on the remote tip, squashing all commits after the
+	// oldest one. We fetch the latest tip from the remote via HEAD, in case the
+	// local clone is not up to date.
+	if _, err := run(ctx, "git", "fetch", "--quiet", cfg.githubURL, "HEAD"); err != nil {
+		return err
+	}
+	if _, err := run(ctx, "git",
+		"-c", "core.editor=cat",
+		"-c", `sequence.editor=sed -i -e '2,$s/^pick/squash/'`,
+		"rebase", "--interactive", "FETCH_HEAD",
+	); err != nil {
+		return err
+	}
+	log.Printf("rebased and squashed on the latest remote tip")
+
+	// TODO: fix up common commit message issues, especially when squashing, in Go code.
+	// TODO: automate adding "Closes #PR as merged.".
+
+	// TODO: add a header (Change-Id or GitOrigin-RevId? see
+	// https://cue-review.googlesource.com/c/cue/+/9781) to ensure that we don't
+	// import the same PR multiple times into different CLs.
+
+	// Amend the squashed commit message manually.
+	// More often than not, we'll want to tweak commit messages to follow
+	// https://github.com/cue-lang/cue/blob/HEAD/doc/contribute.md#good-commit-messages.
+	// Moreover, if we squashed commits, a human needs to merge or discard their messages.
+	// Note that we forward stdin/out/err for terminal editors like vim.
+	// TODO: also add the PR title and description above the commit messages if
+	// we squashed, because some people put the info there.
+	log.Printf("opening editor to fix up commit message...")
+	editCmd := exec.CommandContext(context.Background(), "git", "commit", "--quiet", "--amend")
+	editCmd.Stdin = os.Stdin
+	editCmd.Stdout = os.Stdout
+	editCmd.Stderr = os.Stderr
+	if err := editCmd.Run(); err != nil {
+		return err
+	}
+
+	log.Printf("when you're happy with the commit, run: git-codereview mail")
 	return nil
 }
 
-type importPRPayload struct {
-	PR int `json:"pr"`
-}
-
-func buildImportPRPayload(msg string, payload importPRPayload) (github.DispatchRequestOptions, error) {
-	return buildDispatchPayload(msg, eventTypeImportPR, payload)
+func run(ctx context.Context, name string, args ...string) (string, error) {
+	cmd := exec.CommandContext(ctx, name, args...)
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		return "", fmt.Errorf("%v: %s", err, out)
+	}
+	return string(out), err
 }
