cue-lang/contrib-tools

commit a4e02960fb367f466b3d93b015d89483624a1021

cmd/cueckoo: drop dependency on go-git (#41)
go-git does not fully support worktrees.

This means that, for example, calling cueckoo runtrybot from within a
git worktree fails.

This is very annoying.

Fix that by dropping our dependency on this module.

Tested locally within a worktree setting.

cmd/cueckoo/cmd/cltrigger.go
@@ -22,8 +22,6 @@ import (
 	"strings"
 	"sync"
 
-	"github.com/go-git/go-git/v5/plumbing"
-	"github.com/go-git/go-git/v5/plumbing/object"
 	"golang.org/x/build/gerrit"
 )
 
@@ -85,19 +83,9 @@ func (c *cltrigger) deriveChangeIDs(args map[string]bool) (res []revision, err e
 	}
 
 	// Calculate the list of commits that are pending
-	commitList, err := run(ctx, "git", "log", "--pretty=format:%H", "--no-patch", fmt.Sprintf("%s..HEAD", strings.TrimSpace(bp)))
+	pendingCommits, err := resolveCommits(ctx, fmt.Sprintf("%s..HEAD", strings.TrimSpace(bp)))
 	if err != nil {
 		return nil, err
-	}
-
-	var pendingCommits []*object.Commit
-	for _, line := range strings.Split(commitList, "\n") {
-		h := strings.TrimSpace(line)
-		commit, err := c.cfg.repo.CommitObject(plumbing.NewHash(h))
-		if err != nil {
-			return nil, fmt.Errorf("failed to derive commit from %q: %v", h, err)
-		}
-		pendingCommits = append(pendingCommits, commit)
 	}
 
 	if len(pendingCommits) == 0 {
@@ -109,8 +97,8 @@ func (c *cltrigger) deriveChangeIDs(args map[string]bool) (res []revision, err e
 	if !args["HEAD"] && len(pendingCommits) > 1 && len(args) == 0 {
 		return nil, fmt.Errorf("must specify commits as arguments or use HEAD for everything")
 	}
-	addRevision := func(pc *object.Commit) error {
-		changeID, err := getChangeIDFromCommitMsg(pc.Message)
+	addRevision := func(pc commit) error {
+		changeID, err := getChangeIDFromCommitMsg(pc.body)
 		if err != nil {
 			return fmt.Errorf("failed to derive change ID: %v", err)
 		}
@@ -132,7 +120,7 @@ func (c *cltrigger) deriveChangeIDs(args map[string]bool) (res []revision, err e
 
 		res = append(res, revision{
 			changeID: changeID,
-			revision: pc.Hash.String(),
+			revision: pc.hash,
 		})
 		return nil
 	}
@@ -145,26 +133,24 @@ func (c *cltrigger) deriveChangeIDs(args map[string]bool) (res []revision, err e
 		}
 	} else {
 		// To unique the list of commits for which we submit requests
-		seen := make(map[plumbing.Hash]bool)
+		seen := make(map[string]bool)
 
 		// We verify each of the arguments
 	EachArg:
 		for h := range args {
 			// Resolve the arg and ensure we have a matching pending commit
-			hash, err := c.cfg.repo.ResolveRevision(plumbing.Revision(h))
-			if err != nil {
+			// and ensure we have a single one
+			commits, err := resolveCommits(ctx, h)
+			if err != nil || len(commits) != 1 {
 				return nil, fmt.Errorf("failed to resolve revision %q", h)
 			}
-			if seen[*hash] {
+			commit := commits[0]
+			if seen[commit.hash] {
 				continue
 			}
-			seen[*hash] = true
-			commit, err := c.cfg.repo.CommitObject(*hash)
-			if err != nil {
-				return nil, fmt.Errorf("failed to derive commit from %q: %v", h, err)
-			}
+			seen[commit.hash] = true
 			for _, pc := range pendingCommits {
-				if commit.Hash == pc.Hash {
+				if commit.hash == pc.hash {
 					if err := addRevision(pc); err != nil {
 						return nil, err
 					}
@@ -175,6 +161,37 @@ func (c *cltrigger) deriveChangeIDs(args map[string]bool) (res []revision, err e
 		}
 	}
 	return
+}
+
+type commit struct {
+	hash string
+	body string
+}
+
+func resolveCommits(ctx context.Context, args ...string) ([]commit, error) {
+
+	// Log a stream of commits, separated by NUL, with a single space
+	// separating the commit hash and commit message body
+	cmd := append([]string{"git", "log", "-z", "--pretty=format:%H %B", "--no-patch"}, args...)
+	commitStream, err := run(ctx, cmd[0], cmd[1:]...)
+	if err != nil {
+		return nil, err
+	}
+
+	var commits []commit
+
+	// The results are NUL-separated thanks to -z
+	commitList := strings.Split(commitStream, "\x00")
+
+	for _, commitBlob := range commitList {
+		parts := strings.SplitN(commitBlob, " ", 2)
+		commits = append(commits, commit{
+			hash: parts[0],
+			body: parts[1],
+		})
+	}
+
+	return commits, nil
 }
 
 type revision struct {

cmd/cueckoo/cmd/importpr.go
@@ -47,7 +47,7 @@ func importPRDef(c *Command, args []string) error {
 	log.SetPrefix("[importpr] ")
 	log.SetFlags(0) // no timestamps, as they aren't very useful
 
-	cfg, err := loadConfig()
+	cfg, err := loadConfig(c.Context())
 	if err != nil {
 		return err
 	}
@@ -66,10 +66,7 @@ func importPRDef(c *Command, args []string) error {
 
 	branchName := fmt.Sprintf("importpr-%d", prNumber)
 
-	// TODO: Note that mainErr's ctx is not wired here.
-	// We should wire it up and use it for e.g. FetchContext.
-	// For now, use a hard-coded timeout of 10s.
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, cancel := context.WithTimeout(c.Context(), 10*time.Second)
 	defer cancel()
 
 	pr, _, err := cfg.githubClient.PullRequests.Get(context.Background(), cfg.githubOwner, cfg.githubRepo, prNumber)

cmd/cueckoo/cmd/runtrybot.go
@@ -64,7 +64,7 @@ If the --nounity flag is provided, only a trybot run is triggered.
 }
 
 func runtrybotDef(cmd *Command, args []string) error {
-	cfg, err := loadConfig()
+	cfg, err := loadConfig(cmd.Context())
 	if err != nil {
 		return err
 	}

cmd/cueckoo/cmd/unity.go
@@ -57,7 +57,7 @@ access token only requires "public_repo" scope.
 }
 
 func unityDef(cmd *Command, args []string) error {
-	cfg, err := loadConfig()
+	cfg, err := loadConfig(cmd.Context())
 	if err != nil {
 		return err
 	}

cmd/cueckoo/cmd/util.go
@@ -20,9 +20,9 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"strings"
 
 	"github.com/cue-sh/tools/internal/codereviewcfg"
-	"github.com/go-git/go-git/v5"
 	"github.com/google/go-github/v31/github"
 	"golang.org/x/build/gerrit"
 )
@@ -48,9 +48,6 @@ type repositoryDispatchPayload struct {
 // directory. Put another way, cueckoo needs to be run from within the main
 // cue repo.
 type config struct {
-	// repo is the local cue repository
-	repo *git.Repository
-
 	// gerritURL is the URL of the Gerrit instance
 	gerritURL string
 
@@ -78,22 +75,16 @@ type config struct {
 
 // loadConfig loads the repository configuration from codereview.cfg, using
 // gh as the key to find the relevant GitHub information
-func loadConfig() (*config, error) {
+func loadConfig(ctx context.Context) (*config, error) {
 	var res config
-	rep, err := git.PlainOpenWithOptions(".", &git.PlainOpenOptions{
-		DetectDotGit: true,
-	})
-	if err != nil {
-		return nil, fmt.Errorf("failed to find git repository: %v", err)
-	}
-	res.repo = rep
 
-	wt, err := rep.Worktree()
+	// Determine git root directory. Note it will have trailing newline
+	gitRoot, err := run(ctx, "git", "rev-parse", "--show-toplevel")
 	if err != nil {
-		return nil, fmt.Errorf("failed to get worktree: %v", err)
+		return nil, fmt.Errorf("failed to determine git root: %w", err)
 	}
 
-	cfg, err := codereviewcfg.Config(wt.Filesystem.Root())
+	cfg, err := codereviewcfg.Config(strings.TrimSpace(gitRoot))
 	if err != nil {
 		return nil, fmt.Errorf("failed to load codereview config: %v", err)
 	}

go.mod
@@ -4,7 +4,6 @@ go 1.17
 
 require (
 	cuelang.org/go v0.4.3
-	github.com/go-git/go-git/v5 v5.4.2
 	github.com/google/go-cmp v0.5.8
 	github.com/google/go-github/v31 v31.0.0
 	github.com/shurcooL/graphql v0.0.0-20220520033453-bdb1221e171e
@@ -16,30 +15,21 @@ require (
 )
 
 require (
-	github.com/Microsoft/go-winio v0.4.16 // indirect
-	github.com/ProtonMail/go-crypto v0.0.0-20210428141323-04723f9f07d7 // indirect
-	github.com/acomagu/bufpipe v1.0.3 // indirect
 	github.com/cockroachdb/apd/v2 v2.0.1 // indirect
 	github.com/emicklei/proto v1.6.15 // indirect
-	github.com/emirpasic/gods v1.12.0 // indirect
-	github.com/go-git/gcfg v1.5.0 // indirect
-	github.com/go-git/go-billy/v5 v5.3.1 // indirect
 	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/go-querystring v1.0.0 // indirect
 	github.com/google/uuid v1.2.0 // indirect
-	github.com/imdario/mergo v0.3.12 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
-	github.com/jbenet/go-context v0.0.0-20150711004518-d14ea06fba99 // indirect
-	github.com/kevinburke/ssh_config v0.0.0-20201106050909-4977a11b4351 // indirect
+	github.com/kr/pretty v0.2.1 // indirect
+	github.com/kr/text v0.2.0 // indirect
 	github.com/lib/pq v1.10.2 // indirect
-	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/mpvl/unique v0.0.0-20150818121801-cbe035fff7de // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/protocolbuffers/txtpbfmt v0.0.0-20201118171849-f6a6b3f636fc // indirect
-	github.com/sergi/go-diff v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/xanzy/ssh-agent v0.3.0 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
 	golang.org/x/crypto v0.0.0-20220307211146-efcb8507fb70 // indirect
 	golang.org/x/mod v0.4.2 // indirect
 	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
@@ -48,6 +38,6 @@ require (
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
-	gopkg.in/warnings.v0 v0.1.2 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )

commit f6fe40e9fb31e12ea7845bff31b9b29659a44884

cmd/cueckoo: fix runtrybot taking a list of commits
In a4e0296 we moved away from using go-git to instead use git itself to
resolve commits etc.

This was tested using the special HEAD argument to runtrybot, but not a
specific list of commits.

a4e0296 broke calling runtrybot with a specific commit.

Fix that.

Tested locally with both HEAD and a list of commits.

cmd/cueckoo/cmd/cltrigger.go
@@ -140,7 +140,7 @@ func (c *cltrigger) deriveChangeIDs(args map[string]bool) (res []revision, err e
 		for h := range args {
 			// Resolve the arg and ensure we have a matching pending commit
 			// and ensure we have a single one
-			commits, err := resolveCommits(ctx, h)
+			commits, err := resolveCommits(ctx, "-1", h)
 			if err != nil || len(commits) != 1 {
 				return nil, fmt.Errorf("failed to resolve revision %q", h)
 			}